789e23067891c374634037b7f7086894121bca1d
Sathish <sathish316@gmail.com>
2025-11-22 09:05:50 +0530

    subagent cli skeleton code

--- cli/subagent_command.go
+++ cli/subagent_command.go
@@ -17,3 +17,31 @@ var subagentCmd = &cobra.Command{
 	},
 }
 
+var subagentListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List subagents of a team",
+	Long:  `List subagents of a team`,
+	Run: func(cmd *cobra.Command, args []string) {
+		teamName, _ := cmd.Flags().GetString("team")
+		fmt.Printf("Listing subagents of team %s...\n", teamName)
+	},
+}
+
+var subagentShowCmd = &cobra.Command{
+	Use:   "show",
+	Short: "Show subagent of a team",
+	Long:  `Show subagent of a team`,
+	Run: func(cmd *cobra.Command, args []string) {
+		teamName, _ := cmd.Flags().GetString("team")
+		subagentName, _ := cmd.Flags().GetString("name")
+		fmt.Printf("Showing subagent %s of team %s...\n", subagentName, teamName)
+	},
+}
+
+func init() {
+	subagentListCmd.Flags().StringP("team", "t", "pied-piper", "Team name")
+	subagentShowCmd.Flags().StringP("name", "s", "", "Subagent name")
+	subagentShowCmd.MarkFlagRequired("name")
+	subagentCmd.AddCommand(subagentListCmd)
+	subagentCmd.AddCommand(subagentShowCmd)
+}
